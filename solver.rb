bd1b43ad6a291b88150afb70d634569fbff90eee
Nasir <nasirbhatti.nk@gmail.com>
2023-08-03 17:40:08 +0500

    Fix rubocop linters

--- solver.rb
+++ solver.rb
@@ -12,10 +12,10 @@ class Solver
     word.reverse
   end
 
-  def self.fizzbuzz(n)
-    return 'fizzbuzz' if n % 3 == 0 && n % 5 == 0
-    return 'fizz' if n % 3 == 0
-    return 'buzz' if n % 5 == 0
+  def self.fizzbuzz(num)
+    return 'fizzbuzz' if num % 3 == 0 && num % 5 == 0
+    return 'fizz' if num % 3 == 0
+    return 'buzz' if num % 5 == 0
 
     n.to_s
   end
