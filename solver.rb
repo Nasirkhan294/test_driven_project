9c36424d2e8516f98cbfc21eb169fb4a29e8a27a
Nasir <nasirbhatti.nk@gmail.com>
2023-08-03 16:55:18 +0500

    Add solver file to try the tests

---
+++ solver.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# solver.rb
+class Solver
+  def self.factorial(num)
+    raise ArgumentError, 'Factorial only accepts non-negative integers.' if num.negative?
+
+    return 1 if num.zero?
+
+    (1..num).reduce(:*)
+  end
+end
